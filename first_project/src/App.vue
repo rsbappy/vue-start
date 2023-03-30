2f133d3e070a4c6d252af0180278269c806b7335
rsbappy <bappykha226@gmail.com>
2023-03-30 21:08:37 +0600

    vue js on beforemount and on mounted

--- first_project/src/App.vue
+++ first_project/src/App.vue
@@ -1,36 +1,20 @@
 <template>
-  <h1 v-show="showBtn">{{courseName}}</h1>
-  <p>{{ student }}</p>
-  <button @click="showBtn=!showBtn">show/hide</button>
-<br>
-<input type="number" v-model="count">
-<div>
-  <button @click="increment(1)"> + </button>
-      <h2>  {{ count }} / {{checkOddEven }}</h2>
-  <button @click="decrement(1)"> - </button>
-</div>
+  <h1> {{title}}</h1>
+
 </template>
 <script setup>
-   import {computed, ref, watch} from 'vue';
-   let count = ref(0);
-   let showBtn= ref(false)
-   const checkOddEven  = computed(()=>{
-     return count.value %2 === 0 ? 'even': 'odd';
-   })
-    const  courseName = ref("Vue js  Master Class");
-    const   student = ref("rasbappy");
+  // life cycle hooks
 
-    /* Methods*/
- const increment = amount=> count.value += amount;
- const decrement = amount=> count.value -= amount;
+import { onBeforeMount, onMounted, ref } from 'vue';
+ let title = ref('')
 
+  onBeforeMount(()=>{
+    title.value= 'New Value'
+    console.log('Before Mount');
+  })
 
-    // watch
-  watch (()=> count.value, (newValue, oldValue)=> {
-    console.log('newValue', newValue);
-    console.log('oldValue', oldValue);
-    if (newValue>10) {
-      alert('Counter value  is over tha 10')
-    }
+  onMounted(()=>{
+    title.value= 'Vue js master Class'
+    console.log(' Mounted');
   })
 </script>
